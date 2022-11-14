46814ec7f7cfdce87e486017501920b335ada892
NeverDie <michal.obszan@gmail.com>
2022-11-14 14:06:13 +0000

    Update 4 files

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,9 +1,21 @@
-import Counter from './components/Counter';
-
+import { useSelector } from "react-redux";
+import { Fragment } from "react";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+
+  console.log(auth);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {auth ? <UserProfile /> : <Auth />}
+      <Counter />
+    </Fragment>
   );
 }
 
