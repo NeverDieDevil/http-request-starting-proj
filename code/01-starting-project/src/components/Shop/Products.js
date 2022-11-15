74857af35813c70cc591b590ed4768000c891c9b
NeverDie <michal.obszan@gmail.com>
2022-11-15 13:55:09 +0000

    Update 6 files

--- code/01-starting-project/src/components/Shop/Products.js
+++ code/01-starting-project/src/components/Shop/Products.js
@@ -1,5 +1,20 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First Book",
+    description: "the first book i ever wrote",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "My Second Book",
+    description: "the first book i ever wrote",
+  },
+];
 
 const Products = (props) => {
   return (
@@ -7,9 +22,9 @@ const Products = (props) => {
       <h2>Buy your favorite products</h2>
       <ul>
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+          title={DUMMY_PRODUCTS.title}
+          price={DUMMY_PRODUCTS.price}
+          description={DUMMY_PRODUCTS.description}
         />
       </ul>
     </section>
