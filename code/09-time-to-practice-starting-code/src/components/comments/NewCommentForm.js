8bbcf6bea1f2bcdf4ba25183aa838a34dd07b679
NeverDie <michal.obszan@gmail.com>
2022-11-25 11:19:15 +0000

    Update 9 files

--- code/09-time-to-practice-starting-code/src/components/comments/NewCommentForm.js
+++ code/09-time-to-practice-starting-code/src/components/comments/NewCommentForm.js
@@ -1,26 +1,48 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from "react";
 
-import classes from './NewCommentForm.module.css';
+import { useParams } from "react-router-dom";
+
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+
+import classes from "./NewCommentForm.module.css";
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
+  const params = useParams();
+  const { quoteId } = params;
+  console.log(quoteId);
+  const { sendRequest, status, error } = useHttp(addComment);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
-
+    const enteredText = commentTextRef.current.value;
+    sendRequest({ commentData: enteredText, quoteId });
     // optional: Could validate here
 
     // send comment to server
   };
+  const { onAddedComment } = props;
+  useEffect(() => {
+    if (status === "completed" && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment]);
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
